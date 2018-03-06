aeff0dcf14c88e7efa7a0274b6eca6395fb1f658
lloncar15 <37115497+lloncar15@users.noreply.github.com>
2018-03-06 19:01:47 +0100

    Add files via upload

---
+++ Assets/Scripts/MainMenuController.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class MainMenuController : MonoBehaviour {
+	public Text scoretxt;
+
+	public void Play(){
+		SceneManager.LoadScene ("Main");
+	}
+
+	void Awake(){
+		if(!PlayerPrefs.HasKey("highScore")){
+			PlayerPrefs.SetInt ("highScore", 0);
+		}
+		/*if (!PlayerPrefs.HasKey ("music")) {
+			PlayerPrefs.SetInt ("music", 1);
+		}
+		if (!PlayerPrefs.HasKey ("sound")) {
+			PlayerPrefs.SetInt ("sound", 1);
+		}*/
+
+		string a = PlayerPrefs.GetInt ("highScore").ToString ();
+		scoretxt.text = a;
+	}
+
+	public void QuitGame(){
+		Application.Quit ();
+	}
+}
